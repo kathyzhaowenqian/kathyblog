1767a19e6ef243206e7a5832553902f21c88f0bf
kathyzwq <wzywdq@163.com>
2024-04-16 21:13:21 +0800

    0416-5.

--- kathyblog/wsgi.py
+++ kathyblog/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kathyblog.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kathyblog.publish_settings')
 
 application = get_wsgi_application()
