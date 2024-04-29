3b632aa4dc48b7d33fe9c6c4efc01e05dc57a0e8
kathyzwq <wzywdq@163.com>
2024-04-29 22:35:38 +0800

    0429-7.

--- streamlit/dragongame.py
+++ streamlit/dragongame.py
@@ -19,12 +19,12 @@ import json
 # from django.conf import settings
 
 import streamlit_config as settings
-import streamlit.components.v1 as components
+# import streamlit.components.v1 as components
 
-# Include Google Analytics tracking code
-with open("google_analytics.html", "r") as f:
-    html_code = f.read()
-    components.html(html_code, height=0)
+# # Include Google Analytics tracking code
+# with open("google_analytics.html", "r") as f:
+#     html_code = f.read()
+#     components.html(html_code, height=0)
 
 
 # from config.LLM_API import *
